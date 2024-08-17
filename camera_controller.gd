ae6455ddd2989524e700a17eed480750e3b776cd
Evan <evan.e.butterfield@gmail.com>
2024-08-17 08:03:48 -0700

    Grid Selection

---
+++ camera_controller.gd
@@ -0,0 +1,41 @@
+extends Node3D
+
+@export var grid: GridVisualizer
+@export var selected_grid_mesh_instance: MeshInstance3D
+
+@export var speed: float = 1.0
+
+@export var max_zoom: float = 20.0
+@export var min_zoom: float = 8.0
+@export var zoom_speed: float = 1.0
+
+@onready var camera: Camera3D = $Camera3D
+
+var selected_grid_index: int
+
+func _process(delta: float) -> void:
+	var input: Vector2 = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	position += speed * ((basis.x * input.x) + (basis.z * input.y))
+	
+	if Input.is_action_just_pressed("zoom_in"):
+		position.y -= zoom_speed
+		position.y = max(position.y, min_zoom)
+	
+	if Input.is_action_just_pressed("zoom_out"):
+		position.y += zoom_speed
+		position.y = min(position.y, max_zoom)
+
+func _physics_process(delta: float) -> void:
+	var space_state: PhysicsDirectSpaceState3D = get_world_3d().direct_space_state
+	var mouse_pos: Vector2 = get_viewport().get_mouse_position()
+	
+	var origin: Vector3 = camera.project_ray_origin(mouse_pos)
+	var end: Vector3 = origin + camera.project_ray_normal(mouse_pos) * 100
+	var query: PhysicsRayQueryParameters3D = PhysicsRayQueryParameters3D.create(origin, end, 2)
+	
+	var result: Dictionary = space_state.intersect_ray(query)
+	if result:
+		var ground_position: Vector3 = result["position"]
+		selected_grid_index = grid.get_nearest_grid_index(ground_position.x, ground_position.z)
+		var tile_position: Vector3 = grid.get_nearest_grid_point(selected_grid_index)
+		selected_grid_mesh_instance.position = tile_position
---
+++ grid_visualizer.gd
@@ -0,0 +1,91 @@
+@tool
+class_name GridVisualizer extends Node3D
+
+@export var selected_grid_mesh_instance: MeshInstance3D:
+	set(new_selected_grid_mesh_instance):
+		selected_grid_mesh_instance = new_selected_grid_mesh_instance
+		_on_changed()
+
+@export var grid_dimension: int = 2:
+	set(new_grid_dimension):
+		grid_dimension = new_grid_dimension
+		if grid_dimension < 0:
+			grid_dimension = 0
+		if grid_dimension > 128:
+			grid_dimension = 128
+		
+		_on_changed()
+
+var half_grid_tile_size: float = 0.25
+@export var grid_tile_size: float = 0.5:
+	set(new_grid_tile_size):
+		grid_tile_size = new_grid_tile_size
+		half_grid_tile_size = grid_tile_size / 2
+		_on_changed()
+
+@export var mesh: Mesh:
+	set(new_mesh):
+		if mesh:
+			mesh.free()
+		
+		mesh = new_mesh
+		_on_changed()
+  
+@export var material: StandardMaterial3D:
+	set(new_material):
+		if material:
+			material.free()
+		
+		material = new_material
+		_on_changed()
+
+var a_star: AStar3D = null
+
+func get_tile_position(x: int, y: int) -> Vector3:
+	var new_position: Vector3 = Vector3(x * grid_tile_size, 0, y * grid_tile_size)
+	return new_position
+
+func get_nearest_grid_index(x_pos: float, z_pos: float) -> int:
+	var test_position: Vector3 = Vector3(x_pos, 0, z_pos)
+	var closest_index: int = a_star.get_closest_point(test_position)
+	return closest_index
+
+func get_nearest_grid_point(index: int) -> Vector3:
+	return a_star.get_point_position(index)
+
+func _on_changed() -> void:
+	if a_star:
+		a_star.clear()
+	else:
+		a_star = AStar3D.new()
+	
+	if selected_grid_mesh_instance:
+		selected_grid_mesh_instance.mesh.size.x = grid_tile_size
+		selected_grid_mesh_instance.mesh.size.y = grid_tile_size
+	
+	for child in get_children():
+		child.queue_free()
+	
+	var size: float = grid_dimension * grid_tile_size
+	var half_size: float = size / 2
+	
+	var index: int = 0
+	for y_index in grid_dimension:
+		for x_index in grid_dimension:
+			var mesh_position: Vector3 = get_tile_position(x_index, y_index)
+			mesh_position.x -= half_size
+			mesh_position.z -= half_size
+			
+			var mesh_instance: MeshInstance3D = MeshInstance3D.new()
+			mesh_instance.position = mesh_position
+			mesh_instance.mesh = mesh
+			mesh_instance.material_override = material
+			add_child(mesh_instance)
+			
+			a_star.add_point(index, mesh_position)
+			if (y_index - 1) >= 0:
+				a_star.connect_points(index, index - grid_dimension)
+			if (x_index - 1) >= 0:
+				a_star.connect_points(index, index - 1)
+			
+			index += 1
